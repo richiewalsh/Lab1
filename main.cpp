b5cc7d1d58938bf19f322509455c0b449aa0b27b
richiewalsh <richeywalsh37@gmail.com>
2024-04-27 13:35:36 +0000

    create flag and use to trigger loop
    Signed-off-by: richiewalsh <richeywalsh37@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,19 +1,32 @@
 #include "mbed.h"
+#include <cstdio>
 
-// Initialise button
+// Initialise variables
 InterruptIn btn(PC_13);
+volatile int flag = 0;
 
 // define function for when button has been pressed
 void fall(){
 
-printf("Button has been pressed");
+printf("Button has been pressed\n");
+// cause activation of the if loop in main when function has been triggered
+flag = 1;
 
 }
 
 // main() runs in its own thread in the OS
 int main()
 {
+    // Call function when button is pressed
+    btn.fall(&fall);
     while (true) {
+
+        if(flag == 1){
+
+            printf("Button has been pressed\n");
+            // reset flag
+            flag = 0;
+        }
 
     }
 }
