aaf663ea8b2a4c7256d017733c793ff924cf3fde
richiewalsh <richeywalsh37@gmail.com>
2024-04-27 12:54:54 +0000

    Initialise button and function
    Signed-off-by: richiewalsh <richeywalsh37@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include "mbed.h"
 
+// Initialise button
+InterruptIn btn(PC_13);
+
+// define function for when button has been pressed
+void fall(){
+
+printf("Button has been pressed");
+
+}
+
 // main() runs in its own thread in the OS
 int main()
 {
