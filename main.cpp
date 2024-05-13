d768c3b096d38b32621a8b5b72feea46e1488584
richiewalsh <richeywalsh37@gmail.com>
2024-05-13 18:39:31 +0000

    error
    Signed-off-by: richiewalsh <richeywalsh37@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include "mbed.h"
 #include <cstdio>
+/*
+Error in output:
+++ MbedOS Error Info ++
+Error Status: 0x80010133 Code: 307 Module: 1
+Error Message: Mutex: 0x200015FC, Not allowed in ISR context
+Location: 0x8009371
+Error Value: 0x200015FC
+Current Thread: main Id: 0x20001DB8 Entry: 0x8006943 StackSize: 0x1000 StackMem: 0x20000580 SP: 0x2009FF4C 
+For more info, visit: https://mbed.com/s/error?error=0x80010133&tgt=B_L4S5I_IOT01A
+-- MbedOS Error Info --
 
+*/
 // Initialise variables
 InterruptIn btn(PC_13);
-volatile int flag = 0;
+volatile int flag = 0; //Volatile necessary so that flag value is consistent and can be changed anywhere in the code 
 
 // define function for when button has been pressed
 void fall(){
